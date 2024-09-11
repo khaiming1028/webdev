fbfcd99d44dab748b06021d2a3aab57c4c8e7c1b
khaiming1028 <148465212+khaiming1028@users.noreply.github.com>
2024-09-11 23:58:02 +0800

    added css

--- main.php
+++ main.php
@@ -1,17 +1,5 @@
 <html>
     <head>
-        
-
-    <title>Internship Vacancy Portal</title>
-    <nav id= "navbar" class="navbar">
-        <ul>
-      <li><a class="nav-link scrollto" href="main.php">Home</a></li>
-      <li><a class="nav-link scrollto" href="forum.php">Forum</a></li>
-      <li><a class="nav-link scrollto" href="profile.php">Profile</a></li>
-      <li><a class="nav-link scrollto" href="signup.php">Sign up</a></li>
-      <li><a class="nav-link scrollto" href="#login">Log in</a></li>
-        </ul>
-</nav>
 <!--Bootstrap Link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!--CSS Main Template Link-->
@@ -20,6 +8,49 @@
 
     <!-------------------------------------Start of Body--------------------------------------->
     <body>
+
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="main.php"><span>LOGO</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+
+    <title>Internship Vacancy Portal</title>
+    <nav id= "navbar" class="navbar">
+        <ul>
+      <li><a class="nav-link scrollto active" href="main.php">Home</a></li>
+      <li><a class="nav-link scrollto" href="forum.php">Forum</a></li>
+      <li><a class="nav-link scrollto" href="profile.php">Profile</a></li>
+      <li><a class="nav-link scrollto" href="signup.php">Sign up</a></li>
+      <li><a class="nav-link scrollto" href="#login">Log in</a></li>
+        </ul>
+</nav>
+</header>
+    <!--introduction section-->
+<section id="intro" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>INTI Online Internship Vancancy Portal</h1>
+          <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ex temporibus suscipit beatae aliquid dolor quas dolores quisquam reprehenderit incidunt. Tenetur autem sapiente veritatis accusantium maiores veniam ad quaerat voluptatum.</h2>
+          <div>
+            <a href="#search" class="btn-get-started scrollto">Find Job Now</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 intro-img">
+          <img src="img/job2.jpg" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End intro -->
+   
     <!--search and filter section-->
     <section id="search" class="d-flex align-items-center">
     <div class="container">
@@ -47,36 +78,72 @@
     </section>
    
 
-<!--introduction section-->
-<section class="d-flex align-items-center">
-<div class="container">
 
-<div class="row justify-content-between">
-  <div class="col-lg-5 d-flex align-items-center justify-content-center">
-      <h1>This is a Introduction Section</h1>
-</div>
-</div>
-</div>
-</section>
 
 <!--Jobs option section-->
-    <section class="d-flex align-items-center">
-    <div class ="container">
-        <h1>Jobs available</h1>
-        <div>
-            <p>
-                <a href="job-details.php">Content</a>
-            </p>
-        </div>
+<section id="intro" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row gy-4">
+         <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+             <div>
+                <h2>Search for Jobs</h2>
+               <div class="container">
+                <form class="search-form">
+                <input type="text" value="Search....">
+                <input type="text" value="location">
+                <label for="job-category">Category</label>
+                <select name="status" id="status" placeholder="Job Categories">
+                <option value="success">Engineering</option>
+                <option value="pending">Medical</option>
+                <option value="decline">Information Technology</option>
+            </select>
+            <input type="submit" class="btn btn-info">
+            </form>
+               </div>
+             </div>
+         </div>
+      </div>
     </div>
     </section>
+   
 
     <!--partner section-->
-    <section class="d-flex align-items-center">
-        <div class="container">
-            <h1>Partner Section with sliders</h1>
-        </div>
+    <section id="search" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row gy-4">
+         <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/job.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/job.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+             </div>
+         </div>
+      </div>
+    </div>
     </section>
+   
    
     </body>
     <!-------------------------------------End of Body--------------------------------------->
